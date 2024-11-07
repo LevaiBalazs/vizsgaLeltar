c8e15f778f156181d7aeba834f79b1ab69995274
Lévai Balázs <levaibalazsistvan@ktch.hu>
2024-11-07 12:30:30 +0100

    Creating SpringBoot project

---
+++ school-inventory/src/main/java/hu/kulkerleltar/school_inventory/SchoolInventoryApplication.java
@@ -0,0 +1,13 @@
+package hu.kulkerleltar.school_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolInventoryApplication.class, args);
+	}
+
+}
